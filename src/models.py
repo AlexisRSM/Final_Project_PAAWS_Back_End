947801649282173407eb01821f100114d56b2871
Aléxis Mendes <alexismendes@Alexiss-MacBook-Air.local>
2024-08-01 08:41:33 +0100

    Initial Commit

--- src/models.py
+++ src/models.py
@@ -4,16 +4,49 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    is_active = db.Column(db.Boolean(), default=True, nullable=False)
+    is_admin = db.Column(db.Boolean(), default=False, nullable=False)
+    sponsored_animals = db.relationship('Animal', backref='sponsor', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'<User {self.username}>'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            # do not serialize the password, it's a security breach
+        }
+
+class Animal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    species = db.Column(db.String(100), nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    sponsor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+
+    def __repr__(self):
+        return f'<Animal {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "species": self.species,
+            "gender": self.gender,
+            "description": self.description,
+            "image_file": self.image_file,
+            "sponsor_id": self.sponsor_id,
         }
